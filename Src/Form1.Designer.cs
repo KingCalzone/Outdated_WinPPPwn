fc8a0acf1cae548d2a35b43ae43689163f01a035
KingCalzone <141222373+KingCalzone@users.noreply.github.com>
2024-05-14 16:52:30 +0100

    Initial commit

---
+++ Src/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace PPPwn_Calzone_Extensions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.ControlBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Src/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+namespace PPPwn_Calzone_Extensions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process.Start(Environment.CurrentDirectory + "\\Src\\exe\\PPPwn GUI 1.7.exe"); // IF EVER THERES AN UPDATE, REPLACE THE PROVIDED EXE WITH THE NEW ONE!
+            Thread.Sleep(2000);
+            timer1.Start();
+        }
+        static void EnableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+        static void DisableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+        static void _start()
+        {
+            var a = Process.GetProcessesByName("PPPwn GUI 1.7");
+            if (a.Length == 1)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                var b = Process.GetProcessesByName("PPPwn GUI 1.7"); // IF EVER THERES AN UPDATE, CHANGE THIS TO THE NEW VERSION'S NAME
+                if (b.Length == 1)
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        DisableAdapter("enp0s3"); // CHANGE THIS TO YOUR NETWORK INTERFACE IF NOT DETECTED
+                        Thread.Sleep(2500);
+                        EnableAdapter("enp0s3"); // CHANGE THIS TO YOUR NETWORK INTERFACE IF NOT DETECTED
+                        Thread.Sleep(1500);
+                        int hitman = Process.GetCurrentProcess().Id;
+                        Process.GetProcessById(hitman).Kill();
+                    }
+                    Application.Exit();
+                }
+                else
+                {
+                    SecondConnectionCheck();
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ConnectionCheck();
+        }
+        static void ConnectionCheck()
+        {
+            Thread update = new Thread(ConnectionCheck);
+            var timer = new System.Timers.Timer(3000);
+            timer.Elapsed += (sender, args) => { _start(); };
+            timer.Start();
+        }
+        static void SecondConnectionCheck()
+        {
+            Thread update = new Thread(ConnectionCheck);
+            var timer = new System.Timers.Timer(10000);
+            timer.Elapsed += (sender, args) => { _start(); };
+            timer.Start();
+        }
+    }
+}
